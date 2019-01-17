3ace26dc3ae96417cba289e528d4f848da5fbc77
Filippo Bonfante <bonfantefilippo@gmail.com>
2019-01-18 00:50:11 +0100

    improved amqp changed exchange mqtt pub

--- protocols/amqp/index.js
+++ protocols/amqp/index.js
@@ -1,4 +1,6 @@
 var amqp = require("amqplib");
+var mqtt = require("./mqtt_publisher/mqtt");
+
 require("dotenv").config();
 
 //connection open
@@ -18,7 +20,7 @@ open
     });
   })
   .then(channel => {
-    var exchange = "topic_test"; //coda in base alla targa o misurarilevata
+    var exchange = "smartcar"; //coda in base alla targa o misurarilevata
     return channel
       .assertExchange(exchange, "topic", { durable: false }) //l'exchange non esisterà più al riavvio del broker
       .then(() => {
@@ -27,23 +29,33 @@ open
           .assertQueue(queue, { exclusive: true }) // exclusive true--> scopes the queue to the connection (defaults to false)
           .then((err, q) => {
             console.log("[*] Waiting for measurements");
-            channel.bindQueue(queue, exchange, "water");
+            channel.bindQueue(queue, exchange, "*.*"); //ricevo tipo "ab123cd.water"
 
             channel.consume(
               queue,
               msg => {
-                return new Promise(resolve => {
+                return new Promise((resolve, reject) => {
                   if (msg.content) {
-                      channel.ack(msg);
+                      //console.dir(mqtt.getConnectionStatus());
+                    channel.ack(msg);
                     resolve(msg);
-                  }
+                  }/*else{
+                      var meas = "Errore porco dio";
+                      reject(meas)
+                  }*/
                   //GESTIRE SE C'è CONNESSIONE A MQTT PER PUBBLICARE; FARE UN FILE A PARTE PER MQTT DOVE FARò TUTTA
                   //LA PARTE DI COMUNICAZIONE ANCHE SU DB
                 }).then(res => {
                   console.log(
                     `[x] Received: ${res.content} as ${res.fields.routingKey}.`
                   );
-                });
+                  let rkSplit = res.fields.routingKey.split('.');
+                  var topic = `${res.fields.exchange}/v1/${rkSplit[0]}/${rkSplit[1]}`
+                  
+                  mqtt.publishMessage(topic, res.content)
+                })/*.catch((err)=>{
+                    console.log(err);
+                });*/
               },
               { noAck: false } //mando ack
             );
---
+++ protocols/amqp/mqtt_publisher/mqtt.js
@@ -0,0 +1,44 @@
+var mqtt = require("mqtt");
+
+const connectionOptions = {
+  clientId: "SmartCar_pub",
+  protocolId: "MQTT",
+  protocolVersion: 4
+};
+var client = mqtt.connect(
+  "mqtt://broker.hivemq.com:1883",
+  connectionOptions
+);
+
+var clientStatus = false;
+
+client.on("connect", connack => {
+  if (connack.returnCode != 0) {
+    client.on("error", error => {
+      console.log(error);
+    });
+  }
+  console.log("[MQTT] Client connected and waiting for message...");
+  clientStatus = true;
+});
+
+client.on("offline", ()=>{
+    clientStatus = false;
+})
+
+function getConnectionStatus() {
+    return clientStatus;
+}
+/**
+ * topics: smartcar/v1/identificativo/sensors
+ * topics: smartcar/v1/identificativo/temperature
+ * topic:  smartcar/v1/identificativo/water ecc
+ *  topic:  smartcar/v1/+/+ ecc
+ */
+
+//topic:  smartcar/v1/identificativo/water ecc
+function publishMessage(topic, message){
+    client.publish(topic, message);
+    console.log("Published: "+message.toString())
+}
+module.exports = {getConnectionStatus, publishMessage};
